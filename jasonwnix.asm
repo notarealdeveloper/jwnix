b1a8279196221199cb943971810596d46db34edd
Jason Wilkes <notarealdeveloper@gmail.com>
2018-01-12 13:53:21 -0500

    Commit auto generated from state at 2014-10-18 03:08:45

--- jasonwnix.asm
+++ jasonwnix.asm
@@ -15,11 +15,8 @@
 ; (segment 0x07c0, offset 0), which resolves to the same physical 
 ; address, but can be surprising.
 
-BITS 16
-%define CODESTART   0x7c00  ; where the bios loads our code
-%define PAGESIZE    0x1000  ; The page size: 4092
-%define MBRSIZE     0x0200  ; The size of our boot sector: 512
-%define IDT         0x5000
+ORG     0           ; Not needed
+BITS    16
 
 %macro print 1
     push    eax
@@ -91,43 +88,69 @@ BITS 16
     mov gs, ax
 %endmacro
 
-; Note: cs cannot be changed directly
+
+; IMPORTANT!
+; ==========
+; We can do long calls as follows
+; call 0x07c0:ihandler
+; or long jumps like this
+; jmp 0x07c0:ihandler
+; We can also do both like this
+; push cs ; (or other segment register)
+; push bx ; (or other general register)
+; retf
+
+%define INIT        0x7c00  ; where the bios loads our code
+%define PAGESIZE    0x1000  ; The page size: 4092
+%define MBRSIZE     0x0200  ; The size of our boot sector: 512
+%define IDT         0x5000
+
+
+align 16
 start:
-    ;clear_all_registers
+    clear_all_registers
     cli                                 ; disable hardware interrupts
     mov sp, 0x7c00 + 0x1000 + 0x200     ; set up the stack segment
-    ;mov ax, 0x07c0                     ; set up the data segment
 
-    mov ax, 0x00
-    mov ds, ax
+	;mov ax, 0x07C0		    ; 0x07C0 is where the BIOS loads our code
+	;add ax, 288		    ; 288 = (4096 + 512) / 16 bytes per paragraph
+	;mov ss, ax             ; ss = 0x07C0 + 288 = 2272
+	;mov sp, 4096           ; stack pointer = address number 4096
+
+	;mov ax, 0x07C0		    ; Set data segment to where we're loaded
+	;mov ds, ax
+	;mov es, ax
+
 
     mov     ah, 0x0e
     mov     al, "A"
     int 0x10
 
-    mov dword    [0x80*4],   0x2000
-
-    ; Clear cx
-    mov byte    [0x2000],   0x66
-    mov byte    [0x2001],   0x31
-    mov byte    [0x2002],   0xc9
-    mov byte    [0x2003],   0xcf
+    ; Write a VGA entry
+    mov ax, 0x0200+"X" ; Format of VGA entry is bg-nybl, fg-nybl, char
+    mov bx, 0xb000
+    mov gs, bx
+    mov word [gs:0x8000], ax
 
     mov     ah, 0x0e
     mov     al, "B"
     int 0x10
 
+    ; Clear cx (interrupt handler in machine code)
+    mov dword    [0x2000],   0x90c93166
+    mov dword    [0x2004],   0x90909090
+    mov dword    [0x2008],   0xcf909090
+    mov dword    [0x80*4],   0x2000
+
+
     mov cx, 0x69
-    cmp cx, 0x69
-    jne die
     mov dx, 0x69
-    cmp dx, 0x69
-    jne die
 
     int 0x80
 
     cmp cl, 0x00
     jne die
+
     cmp dl, 0x69
     jne die
 
@@ -149,18 +172,10 @@ start:
     ; (I forget which, but I think one clears dx and one clears dl)
     mov dword   [0x80*4],   0x5000
     ; Code to clear dx
-    mov byte    [0x5000],   0x66
-    mov byte    [0x5001],   0x31
-    mov byte    [0x5002],   0xd2
-    mov byte    [0x5003],   0xcf
-
+    mov dword   [0x5000],   0xcfd23166
 
     mov cl, 0x88
-    cmp cl, 0x88
-    jne die
     mov dl, 0x88
-    cmp dl, 0x88
-    jne die
 
     ; Note: This next line is the ultimate proof that our interrupt
     ; is actually doing something. This jumps into where we began,
@@ -178,25 +193,62 @@ start:
 
     ; print "D"
     ; If we get here, we won!
-    resb 100
     mov     ah, 0x0e
     mov     al, "D"
-    int 0x10
+    int     0x10
+
+
+    mov dword    [0x69*4],   INIT+ihandler
+    mov dword    [0x06*4],   INIT+invalid_opcode
+
+
+    int 0x69        ; Interrupt 0x69:
+
+    ;ud2            ; Generate invalid opcode, to test exception handler!
+    db 0x0f, 0x0b   ; An actual invalid opcode
 
     jmp die
 
 
-interrupt_msg:  db "Haha gotcha bitch! ", 0x0d, 0x0a, 0x00
-
-int_handler:
-    xor dx, dx
+dd 0xefbeadde
+align 16
+ihandler:
+    mov ax, 0x0200+"E" ; Format of VGA entry is bg-nybl, fg-nybl, char
+    mov bx, 0xb000
+    mov gs, bx
+    mov word [gs:0x85a0], ax
+    inc al
+    mov word [gs:0x85a2], ax
+    inc al
+    mov word [gs:0x85a4], ax
+    inc al
+    mov word [gs:0x85a6], ax
+    inc al
+    xor cx, cx
     iret
 
+invalid_opcode:
+    mov ax, 0x0300+"W" ; Format of VGA entry is bg-nybl, fg-nybl, char
+    mov bx, 0xb000
+    mov gs, bx
+    mov word [gs:0x85a8], ax
+    inc al
+    mov word [gs:0x85aa], ax
+    inc al
+    mov word [gs:0x85ac], ax
+    inc al
+    mov word [gs:0x85ae], ax
+    inc al
+    xor cx, cx
+    iret
+
+interrupt_msg:  db "Haha gotcha bitch! ", 0x0d, 0x0a, 0x00
+
 die:
-    jmp $
+    nop
+    jmp die
 
-
-initmsg:    db "Welcome to JasonWnix!", 0x0d, 0x0a, 0x00
+;initmsg:    db "Welcome to JasonWnix!", 0x0d, 0x0a, 0x00
 
 times 510-($-$$) db 0x00 ; pad remainder of boot sector with zeros
 dw 0xAA55                ; the standard pc boot signature;
